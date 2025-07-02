f20bbc0a828bd343001d0709c513e8d8563e3114
isacamp <144910345+isacamp@users.noreply.github.com>
2025-07-02 12:35:49 -0300

    Add files via upload

--- questions.js
+++ questions.js
@@ -1,1 +1,34 @@
-export default
+export default [
+{
+        question: "Qual o nome Inteiro do Luan Santana?",
+        answers: [
+            {Option:"Luan Santana", correct: false},
+            {Option: "Luan Rafael Santana", correct: false},
+            {Option: "Luan Rafael Domingos Santana", correct: true},
+        ]
+},
+{
+        question: "Qual a idade dele?",
+        answers: [
+            {Option:"28", correct: false},
+            {Option: "34", correct: true},
+            {Option: "35", correct: false},
+        ],
+    },
+    {
+        question: "Qual sua data de aniversário?",
+        answers: [
+        {Option:"13 de abril de 1991", correct: false },
+        {Option: "13 de março de 1991", correct: true},
+        {Option: "13 de maio de 1991", correct: false},
+        ],
+    },
+    {
+        question: "Qual o nome de sua esposa?",
+        answers: [
+            {Option: "Jade", correct: true},
+            {Option: "serena", correct: false},
+            {Option: "Izabela", correct: false},
+        ],
+    },
+];
